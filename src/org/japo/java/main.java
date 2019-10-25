500bf9eb2a59414cdd2ef5a0a31c0bdbc8289f71
Manu-P-Z <55140316+Manu-P-Z@users.noreply.github.com>
2019-10-25 19:52:51 +0200

    :tada: Commit Inicial 02403-TallaZapatoAleatorio

---
+++ src/org/japo/java/main.java
@@ -0,0 +1,43 @@
+/* 
+ * Copyright 2019 Manu Portolés Zagalá.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Manu Portolés Zagalá
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static final Random RND = new Random();
+
+    public static void main(String[] args) {
+        final int TALLA_MIN = 20;
+        final int TALLA_MAX = 20;
+
+        int talla;
+
+        talla = RND.nextInt(TALLA_MAX - TALLA_MIN + 1) + TALLA_MIN;
+
+        System.out.printf("%s%d%n", "Talla de zapato.......:", talla);
+
+    }
+
+}
